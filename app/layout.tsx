97fa351ffd4359bb482b4d7b2d3719ba2f0da80d
InakiGT <yarimfuego@gmail.com>
2025-04-22 12:41:00 -0600

    initial commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,16 @@
+import { montserrat } from './ui/fonts';
+import './ui/global.css'
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: {
+    template: '%s | Acme Dashboard',
+    default: 'Acme Dashboard'
+  },
+  description: 'The official Next.js Course Dashboard, built with App Router',
+  metadataBase: new URL('https://next-learn-dashboard.vercel.sh'),
+}
+
 export default function RootLayout({
   children,
 }: {
@@ -5,7 +18,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body className={`${montserrat.className} antialiased`}>{children}</body>
     </html>
   );
 }
